5efe61a0a4514f729570c026ddad3e2b00fa15d0
TheGustaas <gu.santana1296@gmail.com>
2024-01-24 20:28:10 -0300

    organização

---
+++ js/javasctipt.js
@@ -0,0 +1,20 @@
+    function iniciar() {
+      var button = document.getElementById('comeco');
+      var heartContainer = document.getElementById('heart-container');
+      var motivacional = document.getElementById('motivacional');
+
+      // Esconde o botão
+      button.style.display = 'none';
+
+      // Exibe a animação do coração
+      heartContainer.style.opacity = 1;
+      motivacional.style.opacity = 1;
+    }
+
+    const noButton = document.getElementById('no-button');
+
+    noButton.addEventListener('mouseover', () => {
+      noButton.style.position = 'absolute';
+      noButton.style.top = `${Math.floor(Math.random() * 20)}%`;
+      noButton.style.left = `${Math.floor(Math.random() * 80)}%`;
+    });
