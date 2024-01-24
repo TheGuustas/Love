33f40971d1daf164633fd5ad689f94de3ab720fb
TheGustaas <gu.santana1296@gmail.com>
2024-01-24 20:36:31 -0300

    Configuração

--- js/javasctipt.js
+++ js/javasctipt.js
@@ -1,20 +1,20 @@
-    function iniciar() {
-      var button = document.getElementById('comeco');
-      var heartContainer = document.getElementById('heart-container');
-      var motivacional = document.getElementById('motivacional');
+function iniciar() {
+    var button = document.getElementById('comeco');
+    var heartContainer = document.getElementById('heart-container');
+    var motivacional = document.getElementById('motivacional');
 
-      // Esconde o botão
-      button.style.display = 'none';
+    // Esconde o botão
+    button.style.display = 'none';
 
-      // Exibe a animação do coração
-      heartContainer.style.opacity = 1;
-      motivacional.style.opacity = 1;
-    }
+    // Exibe a animação do coração
+    heartContainer.style.opacity = 1;
+    motivacional.style.opacity = 1;
+  }
 
-    const noButton = document.getElementById('no-button');
+  const noButton = document.getElementById('no-button');
 
-    noButton.addEventListener('mouseover', () => {
-      noButton.style.position = 'absolute';
-      noButton.style.top = `${Math.floor(Math.random() * 20)}%`;
-      noButton.style.left = `${Math.floor(Math.random() * 80)}%`;
-    });
+  noButton.addEventListener('mouseover', () => {
+    noButton.style.position = 'absolute';
+    noButton.style.top = `${Math.floor(Math.random() * 20)}%`;
+    noButton.style.left = `${Math.floor(Math.random() * 80)}%`;
+  });
